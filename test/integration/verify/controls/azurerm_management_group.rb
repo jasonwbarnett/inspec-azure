da2266e054fb99f647ca0b848484c5b43cd090de
Jason Barnett <J@sonBarnett.com>
2019-08-13 09:15:09 -0400

    Fill out integration tests

--- test/integration/verify/controls/azurerm_management_group.rb
+++ test/integration/verify/controls/azurerm_management_group.rb
@@ -7,12 +7,12 @@ control 'azurerm_management_group' do
     its('display_name')           { should eq 'my fist management group (name)' }
     its('tenant_id')              { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
     its('parent_name')            { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('parent_id')              { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('parent_display_name')    { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('children_display_names') { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('children_ids')           { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('children_names')         { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('children_roles')         { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('children_types')         { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('parent_id')              { should eq '/providers/Microsoft.Management/managementGroups/ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('parent_display_name')    { should eq 'Tenant Root Group' }
+    its('children_display_names') { should include 'my second one group (name)' }
+    its('children_ids')           { should include '/providers/Microsoft.Management/managementGroups/my-second-one' }
+    its('children_names')         { should include 'my-second-one' }
+    its('children_roles')         { should include '' }
+    its('children_types')         { should include '/providers/Microsoft.Management/managementGroups' }
   end
 end
