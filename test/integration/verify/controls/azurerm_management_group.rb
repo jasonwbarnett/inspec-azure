fa9fc240ee6e645c38fc61dfb4336108fc77e117
Jason Barnett <J@sonBarnett.com>
2019-08-13 08:49:20 -0400

    Fill out integration tests

--- test/integration/verify/controls/azurerm_management_group.rb
+++ test/integration/verify/controls/azurerm_management_group.rb
@@ -1,11 +1,18 @@
 control 'azurerm_management_group' do
   describe azurerm_management_group(group_id: 'my-first-management-group', expand: true, recurse: true) do
-    it                       { should exist }
-    its('display_name')      { should eq 'my fist management group (name)' }
-    its('id')                { should eq '/providers/Microsoft.Management/managementGroups/my-first-management-group' }
-    its('name')              { should eq 'my-first-management-group' }
-    its('tenant_id')         { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('type')              { should eq '/providers/Microsoft.Management/managementGroups' }
-    its('parent_name')       { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    it                            { should exist }
+    its('id')                     { should eq '/providers/Microsoft.Management/managementGroups/my-first-management-group' }
+    its('type')                   { should eq '/providers/Microsoft.Management/managementGroups' }
+    its('name')                   { should eq 'my-first-management-group' }
+    its('display_name')           { should eq 'my fist management group (name)' }
+    its('tenant_id')              { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('parent_name')            { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('parent_id')              { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('parent_display_name')    { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('children_display_names') { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('children_ids')           { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('children_names')         { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('children_roles')         { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('children_types')         { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
   end
 end
