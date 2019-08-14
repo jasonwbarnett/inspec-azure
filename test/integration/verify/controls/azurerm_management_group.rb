7d97120f3491e468ff0a72c594ef70213ac93ccf
Jason Barnett <J@sonBarnett.com>
2019-08-14 09:19:01 -0400

    Update integration tests for management group(s)

--- test/integration/verify/controls/azurerm_management_group.rb
+++ test/integration/verify/controls/azurerm_management_group.rb
@@ -1,18 +1,21 @@
 control 'azurerm_management_group' do
-  describe azurerm_management_group(group_id: 'my-first-management-group', expand: 'children', recurse: true) do
+  describe azurerm_management_group(group_id: 'mg_parent', expand: 'children', recurse: true) do
     it                            { should exist }
-    its('id')                     { should eq '/providers/Microsoft.Management/managementGroups/my-first-management-group' }
+    its('id')                     { should eq '/providers/Microsoft.Management/managementGroups/mg_parent' }
     its('type')                   { should eq '/providers/Microsoft.Management/managementGroups' }
-    its('name')                   { should eq 'my-first-management-group' }
-    its('display_name')           { should eq 'my fist management group (name)' }
-    its('tenant_id')              { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('parent_name')            { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
-    its('parent_id')              { should eq '/providers/Microsoft.Management/managementGroups/ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('name')                   { should eq 'mg_parent' }
+    its('display_name')           { should eq 'Management Group Parent' }
+    # TODO: Figure out how to fill this in dynamically
+    #its('tenant_id')              { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    #its('parent_name')            { should eq 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    #its('parent_id')              { should eq '/providers/Microsoft.Management/managementGroups/ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
     its('parent_display_name')    { should eq 'Tenant Root Group' }
-    its('children_display_names') { should include 'my second one group (name)' }
-    its('children_ids')           { should include '/providers/Microsoft.Management/managementGroups/my-second-one' }
-    its('children_names')         { should include 'my-second-one' }
-    its('children_roles')         { should include '' }
+    its('children_display_names') { should include 'Management Group Child 1' }
+    its('children_display_names') { should include 'Management Group Child 2' }
+    its('children_ids')           { should include '/providers/Microsoft.Management/managementGroups/mg_child_one' }
+    its('children_ids')           { should include '/providers/Microsoft.Management/managementGroups/mg_child_two' }
+    its('children_names')         { should include 'mg_child_one' }
+    its('children_names')         { should include 'mg_child_two' }
     its('children_types')         { should include '/providers/Microsoft.Management/managementGroups' }
   end
 end
--- test/integration/verify/controls/azurerm_management_groups.rb
+++ test/integration/verify/controls/azurerm_management_groups.rb
@@ -1,5 +1,7 @@
 control 'azurerm_management_groups' do
   describe azurerm_management_groups do
-    its('group_names')       { should include 'ab98a3ac-9c20-4b66-8593-1d2d4de0acb5' }
+    its('group_names')       { should include 'mg_parent' }
+    its('group_names')       { should include 'mg_child_one' }
+    its('group_names')       { should include 'mg_child_two' }
   end
 end
